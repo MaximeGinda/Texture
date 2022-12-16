8c47a9d08c710371c6f6b28c3169984b707fd5fb
MaximeGinda <maximeginda@outlook.com>
2022-12-16 01:56:33 +0100

    Update vertex_shader.glsl

--- TP_texture/TP4_transformations/vertex_shader.glsl
+++ TP_texture/TP4_transformations/vertex_shader.glsl
@@ -16,11 +16,11 @@ uniform sampler2D normalMapSampler;
 void main(){
 
         // TODO : Output position of the vertex, in clip space : MVP * position
-        gl_Position = mvp * vec4(vertices_position_modelspace,1);
+        vec4 position = vec4(vertices_position_modelspace,1);
 
-        //position.z = -texture(normalMapSampler,vertexUV).r;
+        position.z = -texture(normalMapSampler,vertexUV).r;
 
-        // gl_Position = position * mvp;
+        gl_Position = mvp * position;
 
         UV = vertexUV;
 }
