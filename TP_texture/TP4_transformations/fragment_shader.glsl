803e507659ee15dbb0a6618eee260009bafae509
MaximeGinda <maximeginda@outlook.com>
2022-12-17 17:42:54 +0100

    Update fragment_shader.glsl

--- TP_texture/TP4_transformations/fragment_shader.glsl
+++ TP_texture/TP4_transformations/fragment_shader.glsl
@@ -18,7 +18,7 @@ void main(){
 
         vec3 objColor = vec3(1,0,0); //texture(textureSampler,UV).rgb;
 
-        vec3 normal = 2 * texture(normalMap, UV).xyz  - 1.0;
+        vec3 normal = 2 * texture(normalMapSampler, UV).xyz  - 1.0;
 
         vec3 lightDirection = normalize(lightPosition - fragPos);
         float diff = max(dot(normal, lightDirection), 0.0);
